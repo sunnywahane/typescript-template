07480d7ba0de1a2b9a2d13bf6488857d9d2408d4
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-30 15:24:07 +0530

    Added test case for checking for the prime factors of 5

--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,18 +1,23 @@
 import { getPrimeFactorsOf } from '../../src/Dummy'
 
 describe("Test for getPrimeFactorsOf method", () => {
-    it("should return 2", () => {
+    it("getPrimeFactorsOf 2 should return 2", () => {
         const output = getPrimeFactorsOf(2)
         expect(output).toEqual([2])
     })
 
-    it("should return 3", () => {
+    it("getPrimeFactorsOf 3 should return 3", () => {
             const output = getPrimeFactorsOf(3)
             expect(output).toEqual([3])
     })
 
-    it("should return [2,2]", () => {
+    it("getPrimeFactorsOf 4 should return [2,2]", () => {
             const output = getPrimeFactorsOf(4)
             expect(output).toEqual([2,2])
+    })
+
+    it("getPrimeFactorsOf 5 should return [5]", () => {
+            const output = getPrimeFactorsOf(5)
+            expect(output).toEqual([5])
     })
 })
