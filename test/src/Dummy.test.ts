3e5c1c89317913217eeed6b7621cf542d597b1ba
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-30 15:04:28 +0530

    Added a test that checks for all the prime number of 3

--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -6,3 +6,10 @@ describe("Prime factors of 2", () => {
         expect(output).toEqual([2])
     })
 })
+
+describe("Prime factors of 3", () => {
+    it("should return 3", () => {
+        const output = getPrimeFactorsOf(3)
+        expect(output).toEqual([3])
+    })
+})
