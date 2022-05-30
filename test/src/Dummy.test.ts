c6d548e3f0aa55f109182fcbe813ab44a03b609c
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-30 14:59:32 +0530

    Chaged the assertion from toBe to toEqual

--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -3,6 +3,6 @@ import { getPrimeFactorsOf } from '../../src/Dummy'
 describe("Prime factors of 2", () => {
     it("should return 2", () => {
         const output = getPrimeFactorsOf(2)
-        expect(output).toBe([2])
+        expect(output).toEqual([2])
     })
 })
