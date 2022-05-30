8d002c554f1d8954261208df8b1fcf4997c55805
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-30 15:39:00 +0530

    Modified the code to work with the newly added test case

--- src/Dummy.ts
+++ src/Dummy.ts
@@ -4,6 +4,10 @@ export const getPrimeFactorsOf = (input: number) => {
         factors.push(2)
         input = input/2
      }
+     if(input%3==0) {
+        factors.push(3)
+        input = input/3
+     }
      if(input!=1)
      factors.push(input)
      return factors
