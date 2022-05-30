42bfd198023e559444f5e94ff4aee24db1aa8fe0
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-30 15:19:14 +0530

    Formatted the file

--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,22 +1,18 @@
 import { getPrimeFactorsOf } from '../../src/Dummy'
 
-describe("Prime factors of 2", () => {
+describe("Test for getPrimeFactorsOf method", () => {
     it("should return 2", () => {
         const output = getPrimeFactorsOf(2)
         expect(output).toEqual([2])
     })
-})
 
-describe("Prime factors of 3", () => {
     it("should return 3", () => {
-        const output = getPrimeFactorsOf(3)
-        expect(output).toEqual([3])
+            const output = getPrimeFactorsOf(3)
+            expect(output).toEqual([3])
     })
-})
 
-describe("Prime factors of 4", () => {
-    it("should return 4", () => {
-        const output = getPrimeFactorsOf(4)
-        expect(output).toEqual([2,2])
+    it("should return [2,2]", () => {
+            const output = getPrimeFactorsOf(4)
+            expect(output).toEqual([2,2])
     })
 })
